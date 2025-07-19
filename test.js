0dd87cb165483a9438f4dee29abd1a3a418de6d2
Shreya Mandal <shreyaatmandal@gmail.com>
2025-07-19 19:37:38 +0530

    initial commit

---
+++ test.js
@@ -0,0 +1,19 @@
+  const apiKey="83df617923d3f08bc0096d33a71ec9a9";
+          const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+          const searchBox= document.querySelector(".search input");
+          const searchBtn= document.querySelector(".search button");
+          async function todaysWeather(city)
+          {
+            const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+            var data= await response.json();
+            console.log(data);
+            document.querySelector(".city").innerHTML=data.name;
+            document.querySelector(".temp").innerHTML=Math.round(data.main.temp)+" °c" ;
+            document.querySelector(".humid").innerHTML=data.main.humidity + "%";
+            document.querySelector(".wind").innerHTML=data.wind.speed+ " km/h";
+            document.querySelector(".sl").innerHTML=data.main.sea_level+ "mtr";
+      
+          }
+          searchBtn.addEventListener("click", ()=>{
+            todaysWeather(searchBox.value);
+          })
